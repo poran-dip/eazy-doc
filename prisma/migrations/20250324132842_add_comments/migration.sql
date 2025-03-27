37c60cd8dcd738307e70453731dbbd069e0df4cc
poran-dip <porandip0@gmail.com>
2025-03-27 21:51:35 +0530

    restart with simplified schema

--- prisma/migrations/20250324132842_add_comments/migration.sql
+++
@@ -1,6 +0,0 @@
--- AlterTable
-ALTER TABLE "Appointment" ADD COLUMN     "appointmentHistory" TEXT[],
-ADD COLUMN     "comments" TEXT,
-ADD COLUMN     "description" TEXT,
-ADD COLUMN     "prescriptions" TEXT[],
-ADD COLUMN     "tests" TEXT[];
